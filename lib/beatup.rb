4a55fed2b0ecc3bb123144b0e17d7a8e102dff74
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-05-25 13:34:36 -0600

    add BeatupListener

--- lib/beatup.rb
+++ lib/beatup.rb
@@ -1,5 +1,20 @@
 require "beatup/version"
 
 module Beatup
+
+  module Listener
+
+    def listen_to klass, event_name, method_name = nil, &callback
+      if method_name && callback
+        raise ArgumentError, "You can't specify both callback and method_name at once!"
+      end
+
+      callbacks = (klass.__BEATUP_CALLBACKS__[event_name] ||= [])
+      callbacks << method_name ?  self.method(method_name) : callback
+      self
+    end
+
+  end
+
   # Your code goes here...
 end
