cb702af228cf7fa2e0d2d83f02e4e23d68c3bd2f
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-06-14 21:20:57 +0900

    impelement triggerable and event easily

--- lib/beatup.rb
+++ lib/beatup.rb
@@ -17,4 +17,50 @@ module Beatup
   end
 
   # Your code goes here...
+
+  module Triggerable
+
+    class << self
+      attr_accessor :__BEATUP_CALLBACKS__
+
+      def included mod
+        mod.__BEATUP_CALLBACKS__ = {}
+      end
+
+    end
+
+    def trigger(event_name, *args)
+      event = Event[self, event_name].freeze
+      self.class.__BEATUP_CALLBACKS__[event_name].each do|callback|
+        callback.call(event, *args)
+      end
+    end
+
+  end
+
+  class Event
+    attr_reader :source, :name
+    def initialize source, name
+      @source = source
+      @name = name
+    end
+
+    def == other
+      @name == other.name && @source == other.source
+    end
+
+    class << self
+      alias [] new
+    end
+  end
+
+  def self.list_callbacks_of(klass, on: nil)
+    if on
+      on_events = Array(on)
+      klass.__BEATUP_CALLBACKS__.values_at(*on_events)
+    else
+      klass.__BEATUP_CALLBACKS__.values
+    end.flatten!
+  end
+
 end
